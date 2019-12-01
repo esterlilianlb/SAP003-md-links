d040c9c3a91e71e60099e7df5fad6a78b9a5584f
esterlilianlb <esterlilian2231@gmail.com>
2019-12-01 14:07:07 -0300

    app ok faltando testes e cli

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,15 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "camelcase": ["error", { "properties": "always" }],
+      "keyword-spacing": ["error", { "before": true }],
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "space-before-blocks": ["error", "always"],
+      "key-spacing": ["error", { "afterColon": true }],
+      "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
+      "no-multiple-empty-lines": ["error", { "max": 1 }]
     }
 };
